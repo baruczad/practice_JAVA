664d870cb03d616f9eacbf97031a83326cec543f
baruczad <baruczadaniel@gmail.com>
2023-12-09 11:05:53 +0100

    edited r/p/s game

--- practice/src/rock_paper_scissors_game.java
+++ practice/src/rock_paper_scissors_game.java
@@ -7,47 +7,56 @@ public class rock_paper_scissors_game {
         // type rock/paper/scissors to play
         // type x to exit game
 
-        String userHand ="";
+        String userHand = "";
+        int roundCounter = 1;
         do {
-        userHand= userHand();
-        if (!userHand.equals("ROCK") && !userHand.equals("PAPER") && !userHand.equals("SCISSORS") && !userHand.equals("X")){
-            System.out.println("INVALID INPUT... NO ROUND PLAYED!");
-        } else if (userHand.equals("X")) {
-            System.out.println("LEAVING GAME...");
-        } else {
-            String computerHand = computerHand();
-            winner(userHand,computerHand);
+            System.out.println("Round: " + roundCounter);
+            userHand = userHand();
+            if (userHand.equals("X")) {
+                System.out.println("LEAVING GAME...");
+            } else {
+                String computerHand = computerHand();
+                winner(userHand, computerHand);
+                roundCounter++;
+            }
+
         }
-        }while (!userHand.equals("X"));
+        while (!userHand.equals("X"));
     }
 
-    public static String userHand(){;
+    public static String userHand() {
         Scanner scan = new Scanner(System.in);
         System.out.println("ROCK...  PAPER...  SCISSORS...");
-        System.out.println("CHOOSE ONE:");
-        String userHand= scan.nextLine().toUpperCase();
+        System.out.println("Choose one:");
+        String userHand = scan.nextLine().toUpperCase();;
+        while (!userHand.equals("ROCK") && !userHand.equals("PAPER") && !userHand.equals("SCISSORS") && !userHand.equals("X")){
+            System.out.println("Wrong input! Waiting for valid data as rock/paper/scissors...");
+        userHand = scan.nextLine().toUpperCase();
+        }
         return userHand;
     }
-    public static String computerHand(){
+
+    public static String computerHand() {
         Random rnd = new Random();
-         int randomChoice = rnd.nextInt(0,3);
-         String computerHand = "";
-         switch (randomChoice){
-             case 0:
-                 computerHand = "ROCK";
-                 break;
-             case 1:
-                 computerHand = "PAPER";
-                 break;
-             case 2:
-                 computerHand = "SCISSORS";
-                 break;
-         }
-        System.out.println("COMPUTER CHOSE: " + computerHand);
+        int randomChoice = rnd.nextInt(0, 3);
+        String computerHand = "";
+        switch (randomChoice) {
+            case 0:
+                computerHand = "ROCK";
+                break;
+            case 1:
+                computerHand = "PAPER";
+                break;
+            case 2:
+                computerHand = "SCISSORS";
+                break;
+        }
+        System.out.println("Computer chose: " + computerHand);
         return computerHand;
     }
-    public static String winner(String userHand, String computerHand){
-        if ((userHand.equals("ROCK") && computerHand.equals("SCISSORS")) || (userHand.equals("PAPER") && computerHand.equals("ROCK")) || (userHand.equals("SCISSORS") || computerHand.equals("PAPER"))){
+
+    public static String winner(String userHand, String computerHand) {
+        if ((userHand.equals("ROCK") && computerHand.equals("SCISSORS")) || (userHand.equals("PAPER") && computerHand.equals("ROCK")) || (userHand.equals("SCISSORS") || computerHand.equals("PAPER"))) {
             System.out.println("USER WINS THIS ROUND");
         } else if (userHand.equals(computerHand)) {
             System.out.println("IT IS A DRAW!");
